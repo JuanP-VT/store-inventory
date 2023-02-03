355c0ceb21c8b8888fb0f47d9af0751729af56e1
Pablo <cv.juanp@gmail.com>
2023-02-03 11:39:51 -0700

    UPD ViewCategorie component

--- src/components/categories/ViewCategorie.tsx
+++ src/components/categories/ViewCategorie.tsx
@@ -1,8 +1,19 @@
 import React from "react";
-
+import { useEffect, useState } from "react";
+import { Categorie } from "../../interfaces";
 type Props = {};
-
+// This component will call the API to fetch and display all categories and display
 function ViewCategorie({}: Props) {
+  //Fetch categorie list and save in state
+  const [categorieList, setCategorieList] = useState<Categorie[]>([]);
+  useEffect(() => {
+    callApi();
+    async function callApi() {
+      const res = await fetch("https://wild-waterfall-1243.fly.dev/categories");
+      const data = await res.json();
+      setCategorieList(data);
+    }
+  }, []);
   return <div>ViewCategorie</div>;
 }
 
