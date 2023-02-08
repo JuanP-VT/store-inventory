c4ea46f5d09d8931dc8a71efc25cbace9f778ccf
Pablo <cv.juanp@gmail.com>
2023-02-08 14:05:12 -0700

    UPD - Description

--- src/components/products/createPagination.ts
+++ src/components/products/createPagination.ts
@@ -1,6 +1,6 @@
 import { modProduct, Product } from "../../interfaces";
 
-// This function splits an array into smaller arrays of the same size
+// This function splits an array of items into groups
 export default function createPagination(
   arrayOfItems: modProduct[],
   itemsPerPage: number
