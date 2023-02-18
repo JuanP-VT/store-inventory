c11f67df87c5f09ee20f37c177173ad8b655f3cb
Pablo <cv.juanp@gmail.com>
2023-02-18 16:54:07 -0700

    remove console log

--- src/components/products/getAllProductsRequest.ts
+++ src/components/products/getAllProductsRequest.ts
@@ -8,7 +8,6 @@ export default async function getAllProductsRequest(
   const productData = (await prodRes.json()) as ApiProduct[];
   const catRes = await fetch("https://wild-waterfall-1243.fly.dev/categories");
   const categoryData = (await catRes.json()) as Category[];
-  console.log(productData, categoryData);
   const modProductList = productData.map((product) => {
     const categoryIconUrl = categoryData.find(
       (cat) => product.category == cat.name
